f4fc1eaad2ac89eda0f546d5ddabc5ec6fbe59fe
Rishabh Panda (He/Him) <80598737+rishabh-panda@users.noreply.github.com>
2021-09-13 14:40:48 +0530

    Update ten_table_reverse.c

--- ten_table_reverse.c
+++ ten_table_reverse.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
     int main() {
- 
+    
+    //number to generate the mathematical table
     int n=10;
-
+        
+    //loop for iteration
     for(int i=n; i>=1; i--)
+    
+    //printing the table values    
     printf("%d X %d = %d\n",n,i,n*i);
  
     return 0;
