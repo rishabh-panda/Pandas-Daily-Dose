3d53f4199791e0a5f0d17b41fdc1f2883ee5c846
Rishabh Panda (He/Him) <80598737+rishabh-panda@users.noreply.github.com>
2021-09-25 01:58:35 +0530

    Create CircleArea.cpp

---
+++ CircleArea.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define pi 3.14
+
+int main() {
+float radius, area;
+
+cout<<"enter radius: ";
+cin>>radius;
+
+area = pi*pow(radius,2);
+cout<<area;
+return 0;
+}
