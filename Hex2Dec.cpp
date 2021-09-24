85638f8da6d316d41afede4a0da1db42948fe6dd
Rishabh Panda (He/Him) <80598737+rishabh-panda@users.noreply.github.com>
2021-09-24 21:38:29 +0530

    Create Hex2Dec.cpp

---
+++ Hex2Dec.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int HextoDec(string HexNum)
+{
+    int PowersOfSixteen = pow(16,0);
+    int HexNumLength = HexNum.length();
+    int DecimalValue = 0;
+    for (int i = HexNumLength-1; i >= 0; i--)
+    {
+        if (HexNum[i] >= '0' && HexNum[i] <= '9')
+        {
+            DecimalValue += (int(HexNum[i] - 48)*PowersOfSixteen);
+            PowersOfSixteen *= 16;
+        }
+
+        else if (HexNum[i] >= 'A' && HexNum[i] <= 'E')
+        {
+            DecimalValue += (int(HexNum[i] - 55)*PowersOfSixteen);
+            PowersOfSixteen *= 16; 
+        }
+        
+        
+    }
+    
+    return DecimalValue;
+}
+
+int main() {
+string num;
+cout<<"enter hex num: ";
+cin>>num;
+cout<<"decimal equivalent: "<<HextoDec(num);
+return 0;
+}
