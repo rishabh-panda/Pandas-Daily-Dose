b59f59bb73a9e7c3d4e79bcc61ad61307db575ea
Rishabh Panda (He/Him) <80598737+rishabh-panda@users.noreply.github.com>
2021-09-25 13:03:24 +0530

    Create AdditionUsingBitwise.cpp

---
+++ AdditionUsingBitwise.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int BinaryAddition(int a, int b)
+{
+    while (b != 0)
+    {
+        // computing remainder
+        unsigned rem = a & b;
+
+        // performing xor optn.
+        a ^= b;
+
+        // final step: left shift
+        b = rem << 1;
+
+    }
+    
+    return a;
+}
+
+int main() {
+
+int a,b;
+cout<<"enter num a: "; cin>>a;
+cout<<"enter num a: "; cin>>b;
+cout<<"sum: "<<BinaryAddition(a,b);
+
+return 0;
+}
