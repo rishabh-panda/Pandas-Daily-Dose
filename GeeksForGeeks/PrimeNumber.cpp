c7c51cee9d1268a3ef844ca9089518c07c0d6590
Rishabh Panda (He/Him) <80598737+rishabh-panda@users.noreply.github.com>
2021-10-21 18:57:35 +0530

    Create PrimeNumber.cpp

---
+++ GeeksForGeeks/PrimeNumber.cpp
@@ -0,0 +1,16 @@
+int isPrime(int N){
+        if(N<2)
+        {
+            return 0;
+        }
+        
+        for(int i=2; i<=sqrt(N); i++)
+        {
+            if(N%i==0)
+            {
+                return 0;
+            }
+            
+        }
+        return 1;
+    }
