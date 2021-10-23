4667b390d2e436ac0c00d9073a9a047c797ba50e
Rishabh Panda (He/Him) <80598737+rishabh-panda@users.noreply.github.com>
2021-10-24 01:14:07 +0530

    Create DoublingTheValue.cpp

---
+++ GeeksForGeeks/DoublingTheValue.cpp
@@ -0,0 +1,11 @@
+long long solve(int n, long long a[], long long b)
+    {
+        for(int i=0; i<n; i++)
+        {
+            if(a[i]==b)
+            {
+                b=2*b;
+            }
+        }
+        return b;
+    }
