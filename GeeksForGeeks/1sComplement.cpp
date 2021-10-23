c14c7195965226562f88106c0060bee116c78ada
Rishabh Panda (He/Him) <80598737+rishabh-panda@users.noreply.github.com>
2021-10-24 01:27:37 +0530

    Create 1sComplement.cpp

---
+++ GeeksForGeeks/1sComplement.cpp
@@ -0,0 +1,11 @@
+string onesComplement(string S,int N){
+        for(int i=0; i<N; i++)
+        {
+            if (S[i]=='1')
+            {
+                S[i]='0';
+            }
+            else S[i]='1';
+        }
+        return S;
+    }
