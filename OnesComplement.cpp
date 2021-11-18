6734bd1bc2cd1180c254236dcd9b2d9d057a4a44
Rishabh Panda <80598737+rishabh-panda@users.noreply.github.com>
2021-11-18 11:10:47 +0530

    Create OnesComplement.cpp

---
+++ OnesComplement.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int OnesComp(int n)
+{
+    //count the number of binary bits
+int x = log2(n) + 1;
+ 
+// Inverting the bits one by one 
+  for (int i = 0; i < x; i++) 
+       n ^= (1<<i);
+    
+    return n;
+}
+
+int main() {
+cout<<OnesComp(44);
+return 0;
+}
